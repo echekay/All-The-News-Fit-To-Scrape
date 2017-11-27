b69e36e0c9febeded76a0de72718c326f4c4d268
Hongsyn <hongsynk@gmail.com>
2017-11-26 22:32:03 -0500

    Added dynamic port to web app.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+var port = process.env.PORT || 3000;
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
